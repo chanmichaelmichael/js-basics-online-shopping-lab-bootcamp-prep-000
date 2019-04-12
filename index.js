cea0555c5807fb4b0f6768ae51cc00322b83507a
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 10:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ function viewCart() {
     var index = 0;
     output = "In your cart, you have ";
     for(let i = 0; i < len - 1; i++){
-    //while(remaining > 1){
       output = output.concat(cart[i].itemName, " at $", cart[i].itemPrice, ", ")
     }
     output = output.concat("and ", cart[len - 1].itemName, " at $", cart[len - 1].itemPrice, ". ")

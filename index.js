78d3f88afd2e8cc7a49fea360be6fd0d8cacf68a
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 10:21:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,6 +68,7 @@ function placeOrder(cardNumber) {
   if(cardNumber){
     output = "Your total cost is $";
     output = output.concat(cart.total(), ", which will be charged to the card ", cardNumber, ".");
+    cart = [];
   }
   else{
     output = "Sorry, we don't have a credit card on file for you.";

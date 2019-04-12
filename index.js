214436a49056e024db5ee8c63693772b941f53b6
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 10:05:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
     var index = 0;
     output = "In your cart, you have ";
     while(remaining > 1){
-      output = output.concat(cart[index].itemName, " at $", cart[index].itemPrice, ".")
+      output = output.concat(cart[index].itemName, " at $", cart[index].itemPrice, ", ")
       remaining--;
     }
   }

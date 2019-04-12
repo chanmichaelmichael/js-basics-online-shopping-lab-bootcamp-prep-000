88eb894a4285b77c7baa5b84e5c9b0e51732d935
Michael Michael <michael.ta.chan@gmail.com>
2019-04-12 09:58:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.item = Math.random()*Math.floor(100);
+ var price = Math.random()*Math.floor(100);
  return `${item} has been added to your cart.`
 }
 
